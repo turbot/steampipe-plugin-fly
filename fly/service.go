6b72b2bc0210cbaec6aa04d8d8c475804694ed89
Ved misra <47312748+misraved@users.noreply.github.com>
2023-12-07 12:30:50 +0530

    Fix the plugin to correctly return results when environment variables are used for authentication (#21)

--- fly/service.go
+++ fly/service.go
@@ -33,13 +33,13 @@ func getClient(ctx context.Context, d *plugin.QueryData) (*flyapi.Client, error)
 		token = *flyConfig.ApiToken
 	}
 
-	// Return if no credential specified
+	// Return if no credential is specified
 	if token == "" {
 		return nil, fmt.Errorf("api_token must be configured")
 	}
 
 	// Start with an empty Fly config
-	config := flyapi.ClientConfig{ApiToken: flyConfig.ApiToken}
+	config := flyapi.ClientConfig{ApiToken: &token}
 
 	// Create the client
 	client, err := flyapi.CreateClient(ctx, config)
