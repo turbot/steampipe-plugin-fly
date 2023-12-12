27524feff1788f21efc2327977c35284fbc86d1d
Madhushree Ray <madhushree@turbot.com>
2023-12-13 00:26:32 +0530

    Remove config schema

--- fly/connection_config.go
+++ fly/connection_config.go
@@ -2,17 +2,10 @@ package fly
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type flyConfig struct {
-	ApiToken *string `cty:"api_token"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"api_token": {
-		Type: schema.TypeString,
-	},
+	ApiToken *string `hcl:"api_token"`
 }
 
 func ConfigInstance() interface{} {
